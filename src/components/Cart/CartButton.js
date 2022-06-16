7f47b2c5e649d00e71d93a1a88cc5f5e4f700370
Shahbaz Mughal <shahbaz.mughal@RWP.ORCALO.LOCAL>
2022-06-16 06:48:37 +0500

    #React redux chapter-19 lecture-3

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
 import classes from './CartButton.module.css';
-
+import { uiActions } from '../../store/ui-slice';
+import {useDispatch} from 'react-redux'
 const CartButton = (props) => {
+  const dispatch=useDispatch();
+  const togglecartHandler = () =>
+  {
+   dispatch(uiActions.toggle())
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={togglecartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
